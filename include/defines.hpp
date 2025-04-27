3f9b43237c50c9c5faf326893b89c65b27f1779b
Legolase <nikita.larsky@gmail.com>
2025-04-27 15:49:16 +0300

    add unified log format

--- include/defines.hpp
+++ include/defines.hpp
@@ -1,9 +1,14 @@
 #ifndef _DEFINES_HPP
 #define _DEFINES_HPP
 
+#include <iostream>
 #include <fmt/format.h>
 
 #define CURRENT_POSITION \
   (fmt::format("{}:{} {}(...)", __FILE__, __LINE__, __FUNCTION__))
+
+#define LOG_INFO()    (std::cout << "   [INFO] ")
+#define LOG_WARNING() (std::cout << "[WARNING] ")
+#define LOG_ERROR()   (std::cout << "  [ERROR] ")
 
 #endif
