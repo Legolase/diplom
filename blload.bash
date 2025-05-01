d83c8117d0ed596ba4d52172d76e1e2c4bbbedb6
Legolase <nikita.larsky@gmail.com>
2025-05-01 15:17:33 +0300

    change project settings

--- blload.bash
+++ blload.bash
@@ -7,7 +7,7 @@ file_name="$(echo $binlog_path_command | rev | cut -d '/' -f1 | rev)"
 
 container_id_command=$(sudo docker container ls | tail -n +2 | grep 'mysql-bl' | cut -d ' ' -f 1)
 
-final_command="sudo docker exec -i $container_id_command cat $binlog_path_command > ./static/binlogs/$file_name"
+final_command="sudo docker exec -i $container_id_command cat $binlog_path_command > ./static/binlog/$file_name"
 
 echo $final_command
 eval "$final_command"
