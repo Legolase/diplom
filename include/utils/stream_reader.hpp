6d6d0cc9aef460a8c8f88296870650b38ef3970c
Legolase <nikita.larsky@gmail.com>
2025-05-29 15:45:23 +0300

    add method isEnd()

--- include/utils/stream_reader.hpp
+++ include/utils/stream_reader.hpp
@@ -10,21 +10,28 @@
 namespace utils {
 
 /**
- * This class is designed to conveniently work with a potentially infinite stream, where
- * we often need to read relative to the current position. The standard library provides
- * the necessary tools for this, but they are often not concise when used.
+ * @brief Class for convenient relative reading from a stream.
+ *
+ * This class wraps around a standard input stream and provides helper methods for reading
+ * binary data relative to the current position. It simplifies the use of standard stream
+ * methods when dealing with binary data.
  */
 struct StreamReader {
-  /// @brief Constructor from the required stream for convenient reading of binary data
-  /// relative to the current position
-  /// @param[in] in The stream from which to read binary data
+
+  /**
+   * @brief Constructor from the required stream for convenient reading of binary data
+   * relative to the current position
+   * @param[in] in The stream from which to read binary data
+   * /
   explicit StreamReader(std::istream& in) noexcept;
 
-  /// @brief Read row data with advance to T value type and return it
-  /// @tparam T is default constructible class
-  /// @returns T value constructed from row binary data relative to current position
-  /// @throws `BadStream` Thrown if `sizeof(T)` more than available to
-  /// read
+  /**
+   * @brief Read row data with advance to T value type and return it
+   * @tparam T is default constructible class
+   * @returns T value constructed from row binary data relative to current position
+   * @throws `BadStream` Thrown if `sizeof(T)` more than available to
+   * read
+   */
   template<typename T>
     requires std::is_default_constructible_v<T>
   T read()
@@ -36,13 +43,15 @@ struct StreamReader {
     return value;
   }
 
-  /// @brief Same as `read()` but without advance
-  /// @tparam T is default constructible class
-  /// @param[in] offset Unsigned offset to jump to before reading the stream
-  /// @returns T value constructed from row binary data relative to current position with
-  /// offset
-  /// @throws `BadStream` Thrown if `offset + sizeof(T)` more than
-  /// available to read
+  /**
+   * @brief Same as `read()` but without advance
+   * @tparam T is default constructible class
+   * @param[in] offset Unsigned offset to jump to before reading the stream
+   * @returns T value constructed from row binary data relative to current position with
+   * offset
+   * @throws `BadStream` Thrown if `offset + sizeof(T)` more than
+   * available to read
+   */
   template<typename T>
     requires std::is_default_constructible_v<T>
   T peek(size_t offset = 0)
@@ -54,16 +63,28 @@ struct StreamReader {
     return value;
   }
 
-  /// @brief Row copy of memory to specified place
-  /// @param[in, out] dest Place to copy sequence of character
-  /// @param[in] size Size of required sequence
+  /**
+   * @brief Row copy of memory to specified place
+   * @param[in, out] dest Place to copy sequence of character
+   * @param[in] size Size of required sequence
+   * @throws `BadStream` Thrown if `size` more than available to read
+   */
   void readCpy(char* dest, size_t size);
 
-  /// @brief Row copy of memory relative to offset to specified place
-  /// @param[in, out] dest Place to copy sequence of character
-  /// @param[in] offset Unsigned offset to jump to before reading the stream
-  /// @param[in] size Size of required sequence
+  /**
+   * @brief Row copy of memory relative to offset to specified place
+   * @param[in, out] dest Place to copy sequence of character
+   * @param[in] offset Unsigned offset to jump to before reading the stream
+   * @param[in] size Size of required sequence
+   * @throws `BadStream` Thrown if `offset + size` more than available to read
+   */
   void peekCpy(char* dest, size_t offset, size_t size);
+
+  /**
+   * @brief Check if the stream has reached its end.
+   * @return true if end of stream, false otherwise
+   */
+  bool isEnd() noexcept;
 
 private:
   /// @brief The stream from which to read binary data
--- src/utils/stream_reader.cpp
+++ src/utils/stream_reader.cpp
@@ -3,8 +3,7 @@
 namespace utils {
 StreamReader::StreamReader(std::istream& in) noexcept :
     stream(in)
-{
-}
+{}
 
 void StreamReader::readCpy(char* dest, size_t size)
 {
@@ -23,4 +22,13 @@ void StreamReader::peekCpy(char* dest, size_t offset, size_t size)
   readCpy(dest, size);
   stream.seekg(start_pos);
 }
+
+bool StreamReader::isEnd() noexcept
+{
+  char buffer;
+  bool result = stream.readsome(&buffer, sizeof(buffer)) == sizeof(buffer);
+  stream.unget();
+  return result;
+}
+
 } // namespace utils
