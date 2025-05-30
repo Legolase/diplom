ff57f72f393d9419dde2f9ab033f707f733f688f
Legolase <nikita.larsky@gmail.com>
2025-05-30 14:50:33 +0300

    add construction of cdc-conveyor

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,38 @@
-#include <iostream>
-
 #include <binlog/binlog_reader.hpp>
+#include <cdc/cdc.hpp>
+#include <iostream>
+#include <sstream>
 
-int main(int argc, char** argv) {
-  std::vector<mysql_binlog::event::BinlogEvent::Ptr> events;
+template<typename T, typename... Args>
+decltype(auto) uptr(Args&&... args)
+{
+  return std::make_unique<T>(std::forward<Args>(args)...);
+}
 
-  return mysql_binlog::reader::read("./binlog/mysql-bin.000003", events);
+int main(int argc, char** argv)
+{
+  auto db_reader_source =
+      uptr<cdc::DBBufferSource>("localhost", "nikita", "biba", "e_store", 3306);
+  auto event_source =
+      uptr<cdc::EventSource>(std::move(db_reader_source), [](const cdc::Binlog& binlog) {
+      });
+
+  auto table_diff_source = uptr<cdc::TableDiffSource>(
+      std::move(event_source),
+      [](const cdc::TableDiff& table_diff) {
+      }
+  );
+
+  std::pmr::synchronized_pool_resource resource;
+  auto otterbrix_consumer =
+      uptr<cdc::OtterBrixConsumerSink>([](const cdc::ExtendedNode&) {
+      });
+  auto otterbrik_diff_sink = uptr<cdc::OtterBrixDiffSink>(std::move(otterbrix_consumer), &resource);
+  auto main_process = uptr<cdc::MainProcess>(std::move(table_diff_source), std::move(otterbrik_diff_sink));
+
+  main_process->process();
+  // const char* file_path = "./static/binlog/mysql-bin.000002";
+  // std::vector<binlog::event::BinlogEvent::UPtr> v;
+
+  // binlog::reader::read(file_path, v);
 }
