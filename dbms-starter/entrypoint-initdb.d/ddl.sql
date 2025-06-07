46028ec90d22dd02e4c99af47b426761b62fb378
Legolase <nikita.larsky@gmail.com>
2025-06-07 16:17:13 +0300

    update sql init file for better tests

--- dbms-starter/entrypoint-initdb.d/ddl.sql
+++ dbms-starter/entrypoint-initdb.d/ddl.sql
@@ -1,233 +1,47 @@
 SET default_storage_engine = INNODB;
-
-CREATE TABLE `e_store`.`brands`(
-    `_id` BIGINT UNSIGNED NOT NULL auto_increment ,
-    `name` VARCHAR(250) NOT NULL ,
-    PRIMARY KEY(`_id`)
+CREATE TABLE `e_store`.`table` (
+  `_id` BIGINT UNSIGNED NOT NULL auto_increment,
+  `s_tinyint` TINYINT,
+  `s_smallint` SMALLINT,
+  `s_medium` MEDIUMINT,
+  `s_int` INT,
+  `s_bigint` BIGINT,
+  `double` DOUBLE,
+  `bool` BOOL,
+  `small_varchar` VARCHAR(20),
+  `big_varchar` VARCHAR(300),
+  `char` CHAR(16),
+  PRIMARY KEY(`_id`)
 );
+-- Вставка краевых значений
+INSERT INTO `e_store`.`table` (
+  `s_tinyint`, `s_smallint`, `s_medium`,
+  `s_int`, `s_bigint`, `double`, `bool`,
+  `small_varchar`, `big_varchar`, `char`
+) VALUES
+  (-128, -32768, -8388608, -2147483648, -9223372036854775808, -1.7e+308, 0, 'min', 'minimal', 'char_min'),
+  (127, 32767, 8388607, 2147483647, 9223372036854775807, 1.7e+308, 1, 'max', 'maximal value test string', 'char_max'),
+  (0, 0, 0, 0, 0, 0.0, 0, '', '', ''),
+  (-1, -1, -1, -1, -1, 0.5, 1, 'neg1', 'negative 1', 'neg'),
+  (64, 1024, 2048, 65536, 999999999, 9.9, 0, 'val', 'value mid', 'mid_val'),
+  (100, 300, 500, 700, 900, 0.0000001, 1, 'a', 'b', 'c');
 
-CREATE TABLE `e_store`.`categories`(
-    `_id` BIGINT UNSIGNED NOT NULL auto_increment ,
-    `name` VARCHAR(250) NOT NULL ,
-    PRIMARY KEY(`_id`)
-);
+-- Обновление одной строки (например, второй)
+UPDATE `e_store`.`table`
+SET
+  `bool` = 0,
+  `small_varchar` = 'upd',
+  `double` = 0.12345
+WHERE `_id` = 6;
 
-INSERT INTO `e_store`.`brands`(`name`)
-VALUES
-    ('Samsung');
+-- Удаление одной строки (например, первой)
+DELETE FROM `e_store`.`table`
+WHERE `_id` = 5;
 
-INSERT INTO `e_store`.`brands`(`name`)
-VALUES
-    ('Nokia');
-
-INSERT INTO `e_store`.`brands`(`name`)
-VALUES
-    ('Canon');
-    
-INSERT INTO `e_store`.`categories`(`name`)
-VALUES
-    ('Television');
-
-INSERT INTO `e_store`.`categories`(`name`)
-VALUES
-    ('Mobile Phone');
-
-INSERT INTO `e_store`.`categories`(`name`)
-VALUES
-    ('Camera');
-    
-    
-CREATE TABLE `e_store`.`products`(
-    `_id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT ,
-    `name` VARCHAR(250) NOT NULL ,
-    `brand_id` BIGINT UNSIGNED NOT NULL ,
-    `category_id` BIGINT UNSIGNED NOT NULL ,
-    PRIMARY KEY(`_id`) ,
-    INDEX `CATEGORY_ID`(`category_id` ASC) ,
-    INDEX `BRAND_ID`(`brand_id` ASC) ,
-    CONSTRAINT `brand_id` FOREIGN KEY(`brand_id`) REFERENCES `e_store`.`brands`(`_id`) ON DELETE RESTRICT ON UPDATE CASCADE ,
-    CONSTRAINT `category_id` FOREIGN KEY(`category_id`) REFERENCES `e_store`.`categories`(`_id`) ON DELETE RESTRICT ON UPDATE CASCADE
-);
-
-
-INSERT INTO `e_store`.`products`(
-    `name` ,
-    `brand_id` ,
-    `category_id`
-)
-VALUES(
-    'Prime' ,
-    '1' ,
-    '1'
-);
-
-INSERT INTO `e_store`.`products`(
-    `name` ,
-    `brand_id` ,
-    `category_id`
-)
-VALUES(
-    'Octoview' ,
-    '1' ,
-    '1'
-);
-
-INSERT INTO `e_store`.`products`(
-    `name` ,
-    `brand_id` ,
-    `category_id`
-)
-VALUES(
-    'Dreamer' ,
-    '1' ,
-    '1'
-);
-
-INSERT INTO `e_store`.`products`(
-    `name` ,
-    `brand_id` ,
-    `category_id`
-)
-VALUES(
-    'Bravia' ,
-    '1' ,
-    '1'
-);
-
-INSERT INTO `e_store`.`products`(
-    `name` ,
-    `brand_id` ,
-    `category_id`
-)
-VALUES(
-    'Proton' ,
-    '1' ,
-    '1'
-);
-
-INSERT INTO `e_store`.`products`(
-    `name` ,
-    `brand_id` ,
-    `category_id`
-)
-VALUES(
-    'Desire' ,
-    '2' ,
-    '2'
-);
-
-INSERT INTO `e_store`.`products`(
-    `name` ,
-    `brand_id` ,
-    `category_id`
-)
-VALUES(
-    'Passion' ,
-    '2' ,
-    '2'
-);
-
-INSERT INTO `e_store`.`products`(
-    `name` ,
-    `brand_id` ,
-    `category_id`
-)
-VALUES(
-    'Emotion' ,
-    '2' ,
-    '2'
-);
-
-INSERT INTO `e_store`.`products`(
-    `name` ,
-    `brand_id` ,
-    `category_id`
-)
-VALUES(
-    'Sensation' ,
-    '2' ,
-    '2'
-);
-
-INSERT INTO `e_store`.`products`(
-    `name` ,
-    `brand_id` ,
-    `category_id`
-)
-VALUES(
-    'Joy' ,
-    '2' ,
-    '2'
-);
-
-INSERT INTO `e_store`.`products`(
-    `name` ,
-    `brand_id` ,
-    `category_id`
-)
-VALUES(
-    'Explorer' ,
-    '3' ,
-    '3'
-);
-
-INSERT INTO `e_store`.`products`(
-    `name` ,
-    `brand_id` ,
-    `category_id`
-)
-VALUES(
-    'Runner' ,
-    '3' ,
-    '3'
-);
-
-INSERT INTO `e_store`.`products`(
-    `name` ,
-    `brand_id` ,
-    `category_id`
-)
-VALUES(
-    'Traveler' ,
-    '3' ,
-    '3'
-);
-
-INSERT INTO `e_store`.`products`(
-    `name` ,
-    `brand_id` ,
-    `category_id`
-)
-VALUES(
-    'Walker' ,
-    '3' ,
-    '3'
-);
-
-INSERT INTO `e_store`.`products`(
-    `name` ,
-    `brand_id` ,
-    `category_id`
-)
-VALUES(
-    'Jumper' ,
-    '3' ,
-    '3'
-);
-
-
-# insert null
-INSERT INTO `e_store`.`products`(
-    `name` ,
-    `brand_id` ,
-    `category_id`
-)
-VALUES(
-    'I am nu;;' ,
-    '3' ,
-    '3'
-);
-
-UPDATE `e_store`.`brands`
-SET `name` = 'Canina'
-WHERE `_id` = 2 OR `_id` = 3;
+-- Добавим ещё одну произвольную строку
+INSERT INTO `e_store`.`table` (
+  `s_tinyint`, `s_smallint`, `s_medium`,
+  `s_int`, `s_bigint`, `double`, `bool`,
+  `small_varchar`, `big_varchar`, `char`
+) VALUES
+  (42, 1024, 123456, 1000000, 1234567890123456, 2.718281828, 1, 'test', 'arbitrary data', 'test_string');
